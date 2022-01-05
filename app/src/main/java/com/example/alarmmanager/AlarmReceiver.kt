0fe12961e9dfd7d512c9af7cd5246328f27100ea
theok <theokhunov@gmail.com>
2022-01-05 11:19:18 +0500

    Simple Alarm Manager App

---
+++ app/src/main/java/com/example/alarmmanager/AlarmReceiver.kt
@@ -0,0 +1,33 @@
+package com.example.alarmmanager
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val i = Intent(context,DestinationActivity::class.java)
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context,0,i,0)
+
+        val builder = NotificationCompat.Builder(context!!,"foxandroid")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Foxandroid Alarm Manager")
+            .setContentText("Subscribe for more android related content")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(123,builder.build())
+
+    }
+}
